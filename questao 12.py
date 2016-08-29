dca29b5b6cb910494a8b84dc952b18df0139cd88
maria clara ferreira rodrigues <mariaclarafr01@gmail.com>
2016-08-29 15:26:54 -0300

    Add files via upload

---
+++ questao 12.py
@@ -0,0 +1,5 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:27:37) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> valor = eval(input("digite um valor: "))
+for num in range(1,11):
+	print(valor," x ",num, "=" ,num*valor)
